59555d457e222cd2788c019269c55070f61effa4
Andrzej Augustyniak <16428137+andrzej-augustyniak@users.noreply.github.com>
2025-12-17 09:14:07 +0100

    chore(scaffolding): Move Vue components out of the folders, and fix naming convention; run eslint

--- src/components/untyped-modules.d.ts
+++ src/components/untyped-modules.d.ts
@@ -1,16 +1,16 @@
 declare module '*.svg' {
-  const content: string;
-  export default content;
+    const content: string;
+    export default content;
 }
 
 declare module '*.png' {
-  const content: string;
-  export default content;
+    const content: string;
+    export default content;
 }
 
 // 💡 Declares any import ending in .scss, .css, etc., as a module with string keys
 // This is necessary if you are using CSS Modules (e.g., import styles from './file.module.scss')
 declare module '*.scss' {
-  const content: Record<string, string>;
-  export default content;
+    const content: Record<string, string>;
+    export default content;
 }
